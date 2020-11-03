fdb577e88086485f3085aaf865bad9b7583c6abe
schuemie <schuemie@ohdsi.org>
2020-11-03 10:29:56 -0500

    Applying OHDSI code style to SQL

--- extras/GetCovariates.sql
+++ extras/GetCovariates.sql
@@ -1,39 +1,47 @@
 -- PBR's script to grab covariates with large difference in prevalance between T and C
-
-select *
-from
-(
-
-select tc.*, c1.covariate_id, c1.covariate_name, tc1.mean as t_mean, tc2.mean as c_mean
-from
-(
-select tcc1.database_id, t1.cohort_id as t_cohort_id, t1.target_name || '-' || t1.subgroup_name as t_cohort_name, c1.cohort_id as c_cohort_id, c1.target_name || '-' || c1.subgroup_name as c_cohort_name, tcc1.cohort_subjects as t_persons, ccc1.cohort_subjects as c_persons
-from scylla.cohort t1
-inner join scylla.cohort c1
-on t1.subgroup_id = c1.subgroup_id
-and t1.cohort_type = 'TwS'
-and c1.cohort_type = 'TwS'
-inner join scylla.cohort_count tcc1
-on t1.cohort_id = tcc1.cohort_id
-inner join scylla.cohort_count ccc1
-on c1.cohort_id = ccc1.cohort_id
-and tcc1.database_id = ccc1.database_id
-where tcc1.cohort_subjects > 140 and ccc1.cohort_subjects > 140
-and t1.cohort_id <> c1.cohort_id
-) tc
-inner join
-scylla.covariate_value tc1
-on tc.t_cohort_id = tc1.cohort_id
-and tc.database_id = tc1.database_id
-and tc1.mean > 0.5
-inner join
-(select * from scylla.covariate where time_window_id in (1,2,3)) c1
-on tc1.covariate_id = c1.covariate_id
-inner join
-scylla.covariate_value tc2
-on tc.c_cohort_id = tc2.cohort_id
-and tc.database_id = tc2.database_id
-and tc1.covariate_id = tc2.covariate_id
-and tc2.mean < 0.5
-where tc1.mean - tc2.mean > 0.5
-) t1
+SELECT *
+FROM (
+	SELECT tc.*,
+		c1.covariate_id,
+		c1.covariate_name,
+		tc1.mean AS t_mean,
+		tc2.mean AS c_mean
+	FROM (
+		SELECT tcc1.database_id,
+			t1.cohort_id AS t_cohort_id,
+			t1.target_name || '-' || t1.subgroup_name AS t_cohort_name,
+			c1.cohort_id AS c_cohort_id,
+			c1.target_name || '-' || c1.subgroup_name AS c_cohort_name,
+			tcc1.cohort_subjects AS t_persons,
+			ccc1.cohort_subjects AS c_persons
+		FROM scylla.cohort t1
+		INNER JOIN scylla.cohort c1
+			ON t1.subgroup_id = c1.subgroup_id
+				AND t1.cohort_type = 'TwS'
+				AND c1.cohort_type = 'TwS'
+		INNER JOIN scylla.cohort_count tcc1
+			ON t1.cohort_id = tcc1.cohort_id
+		INNER JOIN scylla.cohort_count ccc1
+			ON c1.cohort_id = ccc1.cohort_id
+				AND tcc1.database_id = ccc1.database_id
+		WHERE tcc1.cohort_subjects > 140
+			AND ccc1.cohort_subjects > 140
+			AND t1.cohort_id <> c1.cohort_id
+		) tc
+	INNER JOIN scylla.covariate_value tc1
+		ON tc.t_cohort_id = tc1.cohort_id
+			AND tc.database_id = tc1.database_id
+			AND tc1.mean > 0.5
+	INNER JOIN (
+		SELECT *
+		FROM scylla.covariate
+		WHERE time_window_id IN (1, 2, 3)
+		) c1
+		ON tc1.covariate_id = c1.covariate_id
+	INNER JOIN scylla.covariate_value tc2
+		ON tc.c_cohort_id = tc2.cohort_id
+			AND tc.database_id = tc2.database_id
+			AND tc1.covariate_id = tc2.covariate_id
+			AND tc2.mean < 0.5
+	WHERE tc1.mean - tc2.mean > 0.5
+	) t1
