d25a01007eb4fa378502d5586a16744cc1ed6ea3
Schuemie <MSCHUEMI@its.jnj.com>
2020-11-23 09:21:37 -0500

    Computing imbalanced covariates for review

--- extras/BulkDiagnosticsEval.R
+++ extras/BulkDiagnosticsEval.R
@@ -4,6 +4,32 @@
 library(dplyr)
 outputFolder <- "s:/ScyllaEstimation/OptumEhr"
 
+addCohortNamesAndAnalysisDescriptions <- function(table) {
+  exposures <- readr::read_csv(file.path(outputFolder, "export", "exposure_of_interest.csv"))
+  table <- inner_join(table,
+                      tibble(targetId = exposures$exposure_id,
+                             targetName = exposures$exposure_name),
+                      by = "targetId")
+  table <- inner_join(table,
+                      tibble(comparatorId = exposures$exposure_id,
+                             comparatorName = exposures$exposure_name),
+                      by = "comparatorId")
+  cmAnalysisListFile <- system.file("settings",
+                                    "cmAnalysisList.json",
+                                    package = "ScyllaEstimation")
+  cmAnalysisList <- CohortMethod::loadCmAnalysisList(cmAnalysisListFile)
+  cmAnalysisToRow <- function(cmAnalysis) {
+    row <- tibble(analysisId = cmAnalysis$analysisId,
+                  description = cmAnalysis$description)
+    return(row)
+  }
+  cohortMethodAnalysis <- lapply(cmAnalysisList, cmAnalysisToRow)
+  cohortMethodAnalysis <- do.call("rbind", cohortMethodAnalysis)
+  cohortMethodAnalysis <- unique(cohortMethodAnalysis)
+  table <- inner_join(table, cohortMethodAnalysis, by = "analysisId")
+  return(table)
+}
+
 # Balance ------------------------------------------------------------------------
 balanceFolder <- file.path(outputFolder, "balance")
 files <- list.files(balanceFolder, pattern = "bal_.*.rds", full.names = TRUE)
@@ -39,28 +65,7 @@ getMaxAbsSd <- function(file) {
 }
 stats <- plyr::llply(files, getMaxAbsSd, .progress = "text")
 stats <- bind_rows(stats)
-exposures <- readr::read_csv(file.path(outputFolder, "export", "exposure_of_interest.csv"))
-stats <- inner_join(stats,
-                    tibble(targetId = exposures$exposure_id,
-                           targetName = exposures$exposure_name),
-                    by = "targetId")
-stats <- inner_join(stats,
-                    tibble(comparatorId = exposures$exposure_id,
-                           comparatorName = exposures$exposure_name),
-                    by = "comparatorId")
-cmAnalysisListFile <- system.file("settings",
-                                  "cmAnalysisList.json",
-                                  package = "ScyllaEstimation")
-cmAnalysisList <- CohortMethod::loadCmAnalysisList(cmAnalysisListFile)
-cmAnalysisToRow <- function(cmAnalysis) {
-  row <- tibble(analysisId = cmAnalysis$analysisId,
-                description = cmAnalysis$description)
-  return(row)
-}
-cohortMethodAnalysis <- lapply(cmAnalysisList, cmAnalysisToRow)
-cohortMethodAnalysis <- do.call("rbind", cohortMethodAnalysis)
-cohortMethodAnalysis <- unique(cohortMethodAnalysis)
-stats <- inner_join(stats, cohortMethodAnalysis, by = "analysisId")
+stats <- addCohortNamesAndAnalysisDescriptions(stats)
 readr::write_csv(stats, file.path(outputFolder, sprintf("BalanceOverview_%s.csv", databaseId)))
 x <- stats[stats$maxSd < 0.1, ]
 x$targetName
@@ -87,3 +92,50 @@ readr::write_csv(analysesSum, file.path(outputFolder, sprintf("NegativeControlsO
 x <- analysesSum[analysesSum$nNegativeControls == max(analysesSum$nNegativeControls), ]
 sprintf("targetId = %d; comparatorId = %d; analysisId = %d", x$targetId, x$comparatorId, x$analysisId)
 
+
+# Imbalanced covariates  ------------------------------------------------------------------------
+balanceFolder <- file.path(outputFolder, "balance")
+files <- list.files(balanceFolder, pattern = "bal_.*.rds", full.names = TRUE)
+
+getImbalancedCovariates <- function(file, bounds = c(0.1, 0.2), minCohortSize = 100) {
+  ids <- gsub("^.*bal_t", "", file)
+  targetId <- as.numeric(gsub("_c.*", "", ids))
+  ids <- gsub("^.*_c", "", ids)
+  comparatorId <- as.numeric(gsub("_[aso].*$", "", ids))
+  if (grepl("_s", ids)) {
+    subgroupId <- as.numeric(gsub("^.*_s", "", gsub("_a[0-9]*.rds", "", ids)))
+  } else {
+    subgroupId <- NA
+  }
+  if (grepl("_o", ids)) {
+    outcomeId <- as.numeric(gsub("^.*_o", "", gsub("_a[0-9]*.rds", "", ids)))
+  } else {
+    outcomeId <- NA
+  }
+  ids <- gsub("^.*_a", "", ids)
+  analysisId <- as.numeric(gsub(".rds", "", ids))
+  balance <- readRDS(file)
+  inferredTargetAfterSize <- mean(balance$afterMatchingSumTarget/balance$afterMatchingMeanTarget,
+                                  na.rm = TRUE)
+  inferredComparatorAfterSize <- mean(balance$afterMatchingSumComparator/balance$afterMatchingMeanComparator,
+                                      na.rm = TRUE)
+  if (is.nan(inferredTargetAfterSize ) ||
+      inferredTargetAfterSize > minCohortSize ||
+      is.nan(inferredComparatorAfterSize ) ||
+      inferredComparatorAfterSize < minCohortSize) {
+    return(NULL)
+  }
+  balance <- balance %>%
+    filter(abs(.data$afterMatchingStdDiff) > bounds[1] & abs(.data$afterMatchingStdDiff) < bounds[2])
+  balance$targetId <- rep(targetId, nrow(balance))
+  balance$comparatorId <- rep(comparatorId, nrow(balance))
+  balance$analysisId <- rep(analysisId, nrow(balance))
+  return(balance)
+}
+stats <- plyr::llply(files, getImbalancedCovariates, .progress = "text")
+stats <- bind_rows(stats)
+stats <- addCohortNamesAndAnalysisDescriptions(stats)
+stats$targetShortName <- gsub(" with.*", "", stats$targetName)
+stats$comparatorShortName <- gsub(" with.*", "", stats$comparatorName)
+readr::write_csv(stats, file.path(outputFolder, sprintf("ImbalancedCovariates_%s.csv", databaseId)))
+sum(abs(stats$beforeMatchingStdDiff  > 0.2))
